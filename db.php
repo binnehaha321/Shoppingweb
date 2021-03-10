86051170539df6669c59c2aab70afce64a092ae1
Khanh <tqk792001@gmail.com>
2021-03-10 14:57:20 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,19 @@
 <?php
 
-$link = pg_connect("host=ec2-34-201-248-246.compute-1.amazonaws.com port=5432 dbname=d42h9056e35279 user=rponamderkkkmc password=105ab42d7801e58387e0c25b8415c697368496699430053faf5d1519a028106c sslmode=require") or die("failed");
+$host = "ec2-34-201-248-246.compute-1.amazonaws.com";
+$db = "d42h9056e35279";
+$port = "5432";
+$user = "rponamderkkkmc";
+$pass = "105ab42d7801e58387e0c25b8415c697368496699430053faf5d1519a028106c";
+$ssl = "require";
+
+$link = pg_connect("dbname=$db host=$host port=$port user=$user password=$pass sslmode=$ssl");
+
+if (!$link) {
+    echo "Database connection failed.";
+} else {
+     echo "Database connection success.";
+}
+
 
 ?>
