605f8b553152f66ad7f7c61ba064804ed689093a
Khanh <tqk792001@gmail.com>
2021-03-10 17:02:56 +0700

    Update export_product.php

--- export_product.php
+++ export_product.php
@@ -8,7 +8,7 @@
     $pass = "105ab42d7801e58387e0c25b8415c697368496699430053faf5d1519a028106c";
     $ssl = "require";
 
-    $link = pg_connect("host=".$host." dbname=".$dbname." port=".$port." user=".$user." password=".$pass." sslmode=".$ssl);
+    $link = pg_connect("host=".$host." dbname=".$db." port=".$port." user=".$user." password=".$pass." sslmode=".$ssl);
 
 	$query = 'SELECT proID, proName, proCost, proImg, proDesc FROM "Product" ORDER BY "proID"';
 	
@@ -22,7 +22,7 @@ while ($row = pg_fetch_row($prod)) { ?>
       <img src="<?php echo $row[3];?>">
 
         <h3><?php echo $row[4];?></h3>
-		<h6>Super toys <?php echo $row['2'];?> was made in Vietnam or some where.</h6>
+		<h6>Super toys <?php echo $row[2];?> was made in Vietnam or some where.</h6>
 		<h6><a href="datapro.php?pid=<?php echo $row[0]?>"><button type="button" class="btn btn-info">Buy</button></a></h6>
 
 
