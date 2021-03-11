1c21823d3f7742c74aff278985c39aa13ed43313
Khanh <tqk792001@gmail.com>
2021-03-11 10:40:27 +0700

    Create admin_index.php

---
+++ admin_index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h2>ADMIN PAGE </h2>
+<p>This is function of adminstrator to manage category, product, view order.</p>
+<nav>
+  <ul>
+    <li><a href="select_category.php">Category</a></li>
+    <li><a href="select_product.php">Product</a></li>
+    <li><a href="select_order.php">View Order</a></li>
+  </ul>  
+</nav>
+
+</body>
+</html>
