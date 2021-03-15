9208f6136b6983e8c5e5a8bb027bd9bdcdef8e71
Khanh <tqk792001@gmail.com>
2021-03-15 08:36:45 +0700

    Update payment.php

--- payment.php
+++ payment.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'connectDB.php';
+$conn = connectDB();
+?>
 <html lang="en">
     <head>    
         <link rel="stylesheet" href="payment.css">
@@ -77,10 +81,14 @@
             <b>4</b>
           </span>
         </h4>
-        <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-        <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-        <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-        <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+        <?php
+         $sql= "Select * from product";
+         $result= mysqli_query($conn, $sql);
+         $resultcheck=mysqli_num_rows($result);
+         if ($resultcheck >0) { 
+           while ($row = mysqli_fetch_assoc($result)) {;
+        ?>
+        <p><a href="#"><?php  echo $row['proName']. "<br>";?></a> <span class="price"><?php echo $row['proCost']. "<br>";}?></span></p>
         <hr>
         <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
       </div>
