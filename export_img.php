cbc44326ece65fc1c89f60ac018a397995757e67
Khanh <tqk792001@gmail.com>
2021-03-15 08:22:11 +0700

    Update export_img.php

--- export_img.php
+++ export_img.php
@@ -33,7 +33,7 @@ $conn = connectDB();
                         echo $row['proName']. "<br>";
                         echo"</a>";
                         echo $row['proCost']. "<br>";
-                        echo '<button onclick="window.location.href="payment/php">Check out</button>';
+                        echo '<button onclick="window.location.href="payment.php">Check out</button>';
                         echo "</div>";
                         echo "</div>";
                      $count=$count+1;
